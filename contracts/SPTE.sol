cf4c4cc1573277265143fd435c74c9e650323197
swappet <swap.pet@pm.me>
2020-10-06 16:29:05 +0800

    clear and init:truffle migrate and interact with openzeppelin CLI

--- contracts/SPTE.sol
+++ contracts/SPTE.sol
@@ -3,26 +3,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+contract SPTE {
+    uint256 public value;
 
-contract SPTE is ERC777 {
-
-    /// @notice ERC777 standard which is backwards compatible with ERC20.
-    /// @dev decimals of ERC20 always returns a fixed value of 18 in ERC777
-    /// @param initialSupply assign the initialSupply to the deployer account
-    /// @param defaultOperators If you’re not planning on using operators in your token, you can simply pass an empty array
-    constructor(uint256 initialSupply, address[] memory defaultOperators)
-        public
-        ERC777("SwapPetTokenEgg", "SPTE", defaultOperators)
-    {
-        _mint(msg.sender, initialSupply, "", "");
+    function increase() public {
+      value++;
     }
 }
-
-// contract SPTE {
-//     uint256 public value;
-
-//     function increase() public {
-//       value++;
-//     }
-// }
